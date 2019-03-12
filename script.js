a4a2a0bfe4f7e7d609a8cbdb766643f94f51a0ae
Artem Zharikov <7383125@mail.ru>
2019-03-12 21:12:48 +0300

    12-03-19/1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.onload = changeTop('rgb(255, 255, 255)');
+//document.getElementById('needTop').onresize = console.log('!!!');
 function tryThis() {
     console.log(document.getElementById('try').hidden);
     
@@ -7,3 +9,37 @@ function tryThis() {
         document.getElementById('try').hidden= true ;
     }
 }
+function changeTop(needColor) {
+    //console.log(document.getElementById('needTop').style.borderTopColor);
+    var color;
+    if (needColor == null) {color = document.getElementById('needTop').style.borderTopColor} else {
+        //console.log('!!!: '+document.getElementById('needTop').style.borderTopColor);
+        color = needColor;
+    }
+
+    //console.log(document.getElementById('parentImage').clientHeight);
+    var baseHeight = document.getElementById('parentImage').clientHeight; //высота
+    //console.log(baseHeight);
+    var resultHeight = +baseHeight/100*18.7 +'px solid '+color;
+    document.getElementById('needTop').style.borderTop = resultHeight;
+
+    var baseWight = document.getElementById('parentImage').clientWidth;
+    document.getElementById('needTop').style.width = baseWight/100*91+'px';
+    var resultWight = +baseWight/100*14.3 +'px solid transparent';
+    
+    document.getElementById('needTop').style.borderRight = resultWight;
+}
+
+function mat()  {
+    document.getElementById('needTop').style.opacity=1;
+}
+function glian()  {
+    document.getElementById('needTop').style.opacity=0.85;
+}
+function satin()  {
+    document.getElementById('needTop').style.opacity=0.96;
+}
+  
+  setInterval (function(){  //Тестовое изменение размера через 0.1 сек. 
+    changeTop(null);
+  },100);
